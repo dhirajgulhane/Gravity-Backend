b92a69e2c188811ca020997aba19115dd2d98d18
dhirajgulhane <dhirajgulhane397@gmail.com>
2021-06-30 16:27:18 +0530

    main - postgre database connection properties

--- src/test/java/com/example/angular/AngularApplicationTests.java
+++ src/test/java/com/example/angular/AngularApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.angular;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class AngularApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
